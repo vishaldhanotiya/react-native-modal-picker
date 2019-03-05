1e42058c630d19a7d557f3f8aa0216964c9e7d70
vishal <vishald@neuronsolutions.com>
2019-03-05 18:30:11 +0530

    minor change

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+export {App};
+//AppRegistry.registerComponent(appName, () => App);
