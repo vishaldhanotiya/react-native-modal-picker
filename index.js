356a49cc6446befaae2cea79e2dbb5f48833bd49
vishal <vishald@neuronsolutions.com>
2019-03-06 11:07:35 +0530

    minor change

--- index.js
+++ index.js
@@ -6,6 +6,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import RNPickerDialog from "./src/component/RNModalPicker/RNModalPicker";
 
-export {App};
+
+export {RNPickerDialog};
 //AppRegistry.registerComponent(appName, () => App);
