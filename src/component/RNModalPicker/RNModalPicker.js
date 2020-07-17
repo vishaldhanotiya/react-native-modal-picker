0dde6c208f4a06af19c2f3842a77755b69724d6b
김동근 <gther248@gmail.com>
2020-07-17 20:21:15 +0900

    modify

--- src/component/RNModalPicker/RNModalPicker.js
+++ src/component/RNModalPicker/RNModalPicker.js
@@ -92,6 +92,7 @@ export default class RNModalPicker extends PureComponent {
       >
         <View style={styles.listRowContainerStyle}>
           <Text style={styles.listTextViewStyle}>{item.DAYTIME}</Text>
+          <Text style={styles.listTextViewStyle}>{item.name}</Text>
         </View>
       </TouchableOpacity>
     );
